b20b84eb49fd230a95e6d0cd84665d0e5098e1a4
Tanvir Rahman <mdtanvirt@gmail.com>
2023-05-22 15:00:07 +0600

    Update app

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk 
 
 st.set_page_config(layout="wide")
 
@@ -14,3 +15,41 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 st.title('Project realestate')
+
+data = pd.read_csv('WestEnd_merged_full new.csv')
+st.dataframe(data)
+
+###########
+
+
+chart_data = data.filter(['Lat', 'Lon', 'Distance to Flood', 'Adjacent Bus Station', 'Street_Name'])
+midpoint = (np.average(data["Lat"]), np.average(data["Lon"]))
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=midpoint[0],
+        longitude=midpoint[1],
+        zoom=12,
+        pitch=40,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=chart_data,
+           get_position='[Lon, Lat]',
+           radius=100,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[Lon, Lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=100,
+        ),
+    ],
+))
